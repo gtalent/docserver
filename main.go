14ecddcd3f910282a7b93c1b64540ea423ac94ab
Gary Talent <gtalent2@gmail.com>
2010-11-30 11:18:31 -0600

    Now only parse dml files, all others are returned as they are.

--- main.go
+++ main.go
@@ -1,9 +1,10 @@
 package main
 
 import (
-        "web"
         "io/ioutil"
         "flag"
+	"strings"
+        "web"
 	"dml"
 )
 
@@ -12,7 +13,10 @@ func globalServe(val string) string {
         if err != nil {
 		return "404: File not found: dml-g/" + val
         }
-        return dml.ToHTML(val, string(file))
+	if strings.HasSuffix(val, ".dml") {
+		return dml.ToHTML(val, string(file))
+	}
+	return string(file)
 }
 
 func contextServe(val string) string {
@@ -23,13 +27,16 @@ func contextServe(val string) string {
         if err != nil {
 		return "404: File not found: dml/" + val
         }
-        return dml.ToHTML(val, string(file))
+	if strings.HasSuffix(val, ".dml") {
+		return dml.ToHTML(val, string(file))
+	}
+	return string(file)
 }
 
 func main() {
         global := flag.Bool("global", false, "Allow the server to access any files that the user running it has access to.")
 	flag.Parse()
-        web.Get("/dml/(.*)", contextServe)
+	web.Get("/dml/(.*)", contextServe)
         web.Get("/dml", contextServe)
         if *global {
 		web.Get("/dml-g/(.*)", globalServe)
